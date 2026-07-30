16ad4b597e56f650f4cbeef4356de47dfe2d0d2e
Walid Aoudi <aoudiwalid@yahoo.fr>
2026-07-30 19:24:24 +0200

    modified order.tests.js and lesson-3.yaml

--- frontend/src/pages/Order/Order.test.js
+++ frontend/src/pages/Order/Order.test.js
@@ -37,7 +37,8 @@ describe('Test Order', () => {
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getAllByText('$2.50'))
+        .toHaveLength(1);
     });
   });
 
@@ -63,7 +64,8 @@ describe('Test Order', () => {
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getAllByText('$5.00'))
+        .toHaveLength(1);
     });
   });
 });
